3a647062e1350ef98ea5db17bebd25d17ea10fe3
perham-ha <perham.arsia@ha.ax>
2025-10-23 09:33:57 +0300

    Commit 009 - Phase 2 - Subtraction - Refactor: Updated `CalculatorTest.java` to use ParameterizedTest instead for `subtract_Integer` test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,7 +1,5 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -20,14 +18,15 @@ public class CalculatorTest {
             "5, 0, 5",
             "-5, -10, -15"
     })
-    public void add_IntegerAddition(int a, int b, int expected) {
+    public void add_Integer(int a, int b, int expected) {
         Assertions.assertEquals(expected, calculator.add(a, b));
     }
 
-    @Test
-    @DisplayName("Should subtract two numbers")
-    public void subtract_IntegerSubtraction() {
-        int expected = 2;
-        Assertions.assertEquals(expected, calculator.subtract(5, 3));
+    @ParameterizedTest
+    @CsvSource({
+            "5,3,2"
+    })
+    public void subtract_Integer(int a, int b, int expected) {
+        Assertions.assertEquals(expected, calculator.subtract(a, b));
     }
 }
