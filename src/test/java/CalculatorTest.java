7c67cc748c85f5b513b74acb003471a64c36444e
perham-ha <perham.arsia@ha.ax>
2025-10-23 09:25:11 +0300

    Commit 005 - Phase 2 - Subtraction - Red: Added subtract test to `CalcalulatorTest.java`

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -20,5 +22,12 @@ public class CalculatorTest {
     })
     public void add_IntegerAddition(int a, int b, int expected) {
         Assertions.assertEquals(expected, calculator.add(a, b));
+    }
+
+    @Test
+    @DisplayName("Should subtract two numbers")
+    public void subtract_IntegerSubtraction() {
+        int expected = 4;
+        Assertions.assertEquals(expected, calculator.subtract(5, 9));
     }
 }
