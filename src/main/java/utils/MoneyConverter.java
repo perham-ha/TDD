e585319b2200ff4c45c91a3626a6a93b7098b3ad
perham-ha <perham.arsia@ha.ax>
2025-10-23 10:49:48 +0300

    Commit 031 - MoneyConverter (GREEN)- Should Convert 100 USD to 92 EUR - Added setup process for `MoneyConverterTest.java` - Implemented new class `MoneyConverter.java` to return hardcoded expected value

---
+++ src/main/java/utils/MoneyConverter.java
@@ -0,0 +1,9 @@
+package utils;
+
+import java.math.BigDecimal;
+
+public class MoneyConverter {
+    public BigDecimal usdToEur(BigDecimal ownedCurrency) {
+        return new BigDecimal("92.00");
+    }
+}
--- src/test/java/MoneyConverterTest.java
+++ src/test/java/MoneyConverterTest.java
@@ -1,16 +1,25 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import utils.MoneyConverter;
 
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class MoneyConverterTest {
+    private MoneyConverter converter;
+
+    @BeforeEach
+    public void setUp() {
+        this.converter = new MoneyConverter();
+    }
+
     @Test
     @DisplayName("Should convert 100 USD to 92 EUR")
     void shouldConvert100UsdToEur() {
         BigDecimal usd = new BigDecimal("100.00");
-        BigDecimal result = converter.usdToEuro(usd);
+        BigDecimal result = converter.usdToEur(usd);
         assertThat(result).isEqualByComparingTo("92.00");
     }
 }
