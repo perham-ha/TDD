1ce184049166ee8c2a1b2203db7001a4e3a185c4
perham-ha <perham.arsia@ha.ax>
2025-10-23 10:34:54 +0300

    Commit 029 - MoneyConverter (RED)- Setup of MoneyConverterTest Class - Changed implementations of MoneyConverterTest.java to follow guidelines

--- src/test/java/MoneyConverterTest.java
+++ src/test/java/MoneyConverterTest.java
@@ -1,10 +1,16 @@
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class MoneyConverterTest {
-    private MoneyConverter moneyConverter;
-
-    @BeforeEach
-    public void setUp() {
-        this.moneyConvert = new MoneyConverter();
+    @Test
+    @DisplayName("Should convert 100 USD to 92 EUR")
+    void shouldConvert100UsdToEur() {
+        BigDecimal usd = new BigDecimal("100.00");
+        BigDecimal result = converter.usdToEuro(usd);
+        assertThat(result).isEqualByComparingTo("92.00");
     }
 }
