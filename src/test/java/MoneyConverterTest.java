20dfa13cfa1ebc2f78cb11a5a56a7b6b733fb788
perham-ha <perham.arsia@ha.ax>
2025-10-23 10:30:51 +0300

    Commit 029 - MoneyConverter (RED)- Setup of MoneyConverter Class - Added new `MoneyConverterTest.java` class for future test implementations

---
+++ src/test/java/MoneyConverterTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.BeforeEach;
+
+public class MoneyConverterTest {
+    private MoneyConverter moneyConverter;
+
+    @BeforeEach
+    public void setUp() {
+        this.moneyConvert = new MoneyConverter();
+    }
+}
