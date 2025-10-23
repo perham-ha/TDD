f839a91974b78c7efdefd5a9a143ca852948d965
perham-ha <perham.arsia@ha.ax>
2025-10-23 11:15:03 +0300

    MoneyConverter (GREEN)- Should have two decimals during Conversion - Updated `MoneyConverterTest.java` to use Nested classes - Updated `MoneyConverter.java` to round to two decimals

--- src/main/java/utils/MoneyConverter.java
+++ src/main/java/utils/MoneyConverter.java
@@ -4,6 +4,10 @@ import java.math.BigDecimal;
 
 public class MoneyConverter {
     public BigDecimal usdToEur(BigDecimal ownedCurrency) {
-        return ownedCurrency.multiply(new BigDecimal("0.92"));
+        BigDecimal amount = ownedCurrency.multiply(new BigDecimal("0.92"));
+
+        amount = amount.setScale(2, BigDecimal.ROUND_HALF_UP);
+
+        return amount;
     }
 }
--- src/test/java/MoneyConverterTest.java
+++ src/test/java/MoneyConverterTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import utils.MoneyConverter;
 
@@ -15,27 +16,32 @@ public class MoneyConverterTest {
         this.converter = new MoneyConverter();
     }
 
-    @Test
-    @DisplayName("Should convert 100 USD to 92 EUR")
-    void shouldConvert100UsdToEur() {
-        BigDecimal usd = new BigDecimal("100.00");
-        BigDecimal result = converter.usdToEur(usd);
-        assertThat(result).isEqualByComparingTo("92.00");
-    }
+    @Nested
+    @DisplayName("USD to EUR Conversion")
+    class usdToEurTest {
 
-    @Test
-    @DisplayName("Should convert 200 USD to 184 EUR")
-    void shouldConvert200UsdToEur() {
-        BigDecimal usd = new BigDecimal("200.00");
-        BigDecimal result = converter.usdToEur(usd);
-        assertThat(result).isEqualByComparingTo("184.00");
-    }
+        @Test
+        @DisplayName("Should convert 100 USD to 92 EUR")
+        void shouldConvert100UsdToEur() {
+            BigDecimal usd = new BigDecimal("100.00");
+            BigDecimal result = converter.usdToEur(usd);
+            assertThat(result).isEqualByComparingTo("92.00");
+        }
 
-    @Test
-    @DisplayName("Should have two decimals during conversion")
-    void shouldHaveTwoDecimalsDuringConversion() {
-        BigDecimal usd = new BigDecimal("239.2");
-        BigDecimal result = converter.usdToEur(usd);
-        assertThat(result).isEqualByComparingTo("220.06");
+        @Test
+        @DisplayName("Should convert 200 USD to 184 EUR")
+        void shouldConvert200UsdToEur() {
+            BigDecimal usd = new BigDecimal("200.00");
+            BigDecimal result = converter.usdToEur(usd);
+            assertThat(result).isEqualByComparingTo("184.00");
+        }
+
+        @Test
+        @DisplayName("Should have two decimals during conversion")
+        void shouldHaveTwoDecimalsDuringConversion() {
+            BigDecimal usd = new BigDecimal("239.2");
+            BigDecimal result = converter.usdToEur(usd);
+            assertThat(result).isEqualByComparingTo("220.06");
+        }
     }
 }
