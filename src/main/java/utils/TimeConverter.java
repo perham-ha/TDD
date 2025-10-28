6f1b1e58126d8a4f73e7fb3763dc84677bbf49a4
perham-ha <perham.arsia@ha.ax>
2025-10-28 10:25:45 +0200

    TimeConverter - Should convert 60 seconds to 1 minute (REFACTOR)- - Updated ´TimeConverter´ use better practice

--- src/main/java/utils/TimeConverter.java
+++ src/main/java/utils/TimeConverter.java
@@ -1,8 +1,9 @@
 package utils;
 
 public class TimeConverter {
+    private static final int SECONDS_PER_MINUTE = 60;
 
-    public double secondsToMinutes(int seconds) {
-        return 1.0;
+    public double secondsToMinutes(long seconds) {
+        return (double) seconds / SECONDS_PER_MINUTE;
     }
 }
