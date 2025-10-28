b94306a1d7946dd96c0f068161b6545db8a1f4fb
perham-ha <perham.arsia@ha.ax>
2025-10-28 10:12:14 +0200

    MoneyConverter - shouldThrowExceptionForNegativeUsd (CHANGES)- - Added some comments for clarity to code in `MoneyConverter.java` - Added a `MoneyConverterList` for better tracking of implementation

---
+++ MoneyConverterList.md
@@ -0,0 +1,21 @@
+# List for Calculator Testing
+
+---
+
+## Phase 1: UsdToEur Conversion
+
+* Should convert 100 USD to 92 EUR            [✅]
+* Should have two decimals during conversion  [✅]
+* Should throw exception for negative USD     [✅]
+
+---
+
+## Phase 2: eurToUsd Conversion
+
+* Should convert 92 Eur to 100 USD            [✅]
+* TBD                                         [❌]
+
+---
+
+[✅] - Done \
+[❌] - Not done
--- src/main/java/utils/MoneyConverter.java
+++ src/main/java/utils/MoneyConverter.java
@@ -4,18 +4,23 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class MoneyConverter {
+
+    // Logic that handles usdToEur conversions.
     public BigDecimal usdToEur(BigDecimal ownedCurrency) {
         if (ownedCurrency.compareTo(BigDecimal.ZERO) < 0) {
-            throw new IllegalArgumentException("The owned currency is negative");
+            throw new IllegalArgumentException("The owned currency is negative");   // Throw IllegalArgument at negative numbers
         } else {
             BigDecimal amount = ownedCurrency.multiply(new BigDecimal("0.92"));
 
-            amount = amount.setScale(2, BigDecimal.ROUND_HALF_UP);
+            amount = amount.setScale(2, BigDecimal.ROUND_HALF_UP);  // Rounds to two decimals
             return amount;
         }
     }
 
+    // Logic that handles eurToUsd conversions.
     public BigDecimal eurToUsd(BigDecimal ownedCurrency) {
+        // BigDecimal.divide throws Arithmetic Exception unless Rounding is set
+        // ref. https://stackoverflow.com/questions/12178053/dividing-bigdecimals-results-in-arithmeticexception
         BigDecimal conversationRate = new BigDecimal("1.00").divide(new BigDecimal("0.92"), 10, RoundingMode.HALF_UP);
         BigDecimal amount = ownedCurrency.multiply(conversationRate);
 
