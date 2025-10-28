ddb421c5440274111189f9f53e9191f6f25448c3
perham-ha <perham.arsia@ha.ax>
2025-10-28 09:48:48 +0200

    MoneyConverter - shouldThrowExceptionForNegativeUsd (GREEN)- - Implemented functionality in `MoneyConverter.java` to throw IllegalArgumentException if ownedCurrency is negative

--- src/main/java/utils/MoneyConverter.java
+++ src/main/java/utils/MoneyConverter.java
@@ -5,10 +5,14 @@ import java.math.RoundingMode;
 
 public class MoneyConverter {
     public BigDecimal usdToEur(BigDecimal ownedCurrency) {
-        BigDecimal amount = ownedCurrency.multiply(new BigDecimal("0.92"));
+        if (ownedCurrency.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("The owned currency is negative");
+        } else {
+            BigDecimal amount = ownedCurrency.multiply(new BigDecimal("0.92"));
 
-        amount = amount.setScale(2, BigDecimal.ROUND_HALF_UP);
-        return amount;
+            amount = amount.setScale(2, BigDecimal.ROUND_HALF_UP);
+            return amount;
+        }
     }
 
     public BigDecimal eurToUsd(BigDecimal ownedCurrency) {
