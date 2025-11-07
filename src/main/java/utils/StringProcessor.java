3a57aa117dc1f5958187395a3dee9fc2b35828d4
perham-ha <perham.arsia@ha.ax>
2025-11-07 09:28:40 +0200

    StringProcessor - Should handle a single character (GREEN) - Implemented reverse logic based using arrays, while-loop and single character check in `StringProcessor.java` to get dynamic functionality instead of hardcoded (static) functionality

--- src/main/java/utils/StringProcessor.java
+++ src/main/java/utils/StringProcessor.java
@@ -3,6 +3,28 @@ package utils;
 public class StringProcessor {
 
     public String reverse(String str) {
-        return "dlrowolleh";
+        // Strings with 0 or 1 characters remain unchanged
+        if (str.length() <= 1) {
+            return str;
+        }
+
+        // Converting String (immutable) to array of characters
+        char[] characters = str.toCharArray();
+
+        // Initializing two pointers as start and end of array
+        int start = 0;
+        int end = characters.length - 1;
+
+        // Swap characters from end to start
+        while (start < end) {
+            char temp = characters[start];
+            characters[start] = characters[end];
+            characters[end] = temp;
+            start++;
+            end--;
+        }
+
+        // Constructs and returns a new String from the newly reversed char array
+        return new String(characters);
     }
 }
