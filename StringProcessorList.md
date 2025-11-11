3b39b4193eda2e78c206776a9fd36e057227860a
perham-ha <perham.arsia@ha.ax>
2025-11-11 08:13:11 +0200

    StringProcessor - Implement real `reverse()` with `StringBuilder` (Refactor) - Refactored code to use `reverse()` from StringBuilder instead

--- StringProcessorList.md
+++ StringProcessorList.md
@@ -8,13 +8,12 @@
 * Should handle single character                              [✅]
 * Should handle `Null` and empty Strings                      [✅]
 * Should preserve `spaces` in reversal                        [✅]
-* Implement real `reverse()` with `StringBuilder`             [❌]
+* Implement real `reverse()` with `StringBuilder`             [✅]
 * Should detect simple palindrome                             [❌]
 * Should detect non-palindrome                                [❌]
 * Should handle case-insensitive palindromes                  [❌]
 * Should handle palindromes with spaces                       [❌]
 * Should handle single characters as palindrome               [❌]
 * Should handle null and empty as palindromes (parameterized) [❌]
-
 
 ---
--- src/main/java/utils/StringProcessor.java
+++ src/main/java/utils/StringProcessor.java
@@ -4,28 +4,26 @@ public class StringProcessor {
 
     public String reverse(String str) {
         // Avoiding NullPointerException
-        if(str == null) {return null;}
-
-        // Strings with 0 or 1 characters remain unchanged
-        if (str.length() <= 1) {return str;}
-
-        // Converting String (immutable) to array of characters
-        char[] characters = str.toCharArray();
-
-        // Initializing two pointers as start and end of array
-        int start = 0;
-        int end = characters.length - 1;
-
-        // Swap characters from end to start
-        while (start < end) {
-            char temp = characters[start];
-            characters[start] = characters[end];
-            characters[end] = temp;
-            start++;
-            end--;
+        if (str == null) {
+            return null;
         }
 
+        // Strings with 0 or 1 characters remain unchanged
+        if (str.length() <= 1) {
+            return str;
+        }
+
+        // Create StringBuilder object with string parameter
+        StringBuilder originalString = new StringBuilder(str);
+
+        // Reverse string with built-in `reverse()` method
+        originalString.reverse();
+
+        // Convert StringBuilder object back to a String (I assume we require String
+        // New variable for clarity and easier future handling
+        String reversedString = originalString.toString();
+
         // Constructs and returns a new String from the newly reversed char array
-        return new String(characters);
+        return reversedString;
     }
 }
